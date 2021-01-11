9a8a01acc9a31e25d4551dc771511ebb12279d19
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-11 16:57:17 -0300

    Criado as rotas e funções para realizar o upload dos arquivos no server

--- app/public/src/controller/DropboxController.js
+++ app/public/src/controller/DropboxController.js
@@ -14,8 +14,40 @@ class DropboxController
             this.inputFilesEl.click();
         });
 
-        this.inputFilesEl.addEventListener('change', () => {
+        this.inputFilesEl.addEventListener('change', event => {
+            this.uploadTask(event.target.files);
             this.snackModalEl.style.display = 'block';
         })
+    }
+
+    uploadTask(files)
+    {
+        let promises = [];
+
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST', '/upload');
+
+                ajax.onload = () => {
+                    try
+                    {
+                        resolve(JSON.parse(ajax.responseText));
+                    }
+                    catch(e)
+                    {
+                        reject(e);
+                    }
+                };
+
+                ajax.onerror = e => reject(e);
+                
+                let formData = new FormData();
+                formData.append('input-file', file);
+                ajax.send(formData);
+            }));
+        });
+
+        return Promise.all(promises);
     }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: "./upload", // indica o diretório para onde serão enviados os arquivos
+    keepExtensions: true // Mantém a extensão dos arquivos
+  });
+
+  form.parse(req, (_err, _fields, files) => {
+    res.json({
+      files
+    });
+  });
+  
 });
 
 module.exports = router;
