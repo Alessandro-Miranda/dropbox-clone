aaa30fd0b6a025bd17de477e13c7044b841409ab
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-12 09:11:55 -0300

    Criado o metódo para atualizar a barra de progresso

--- app/public/src/controller/DropboxController.js
+++ app/public/src/controller/DropboxController.js
@@ -5,6 +5,7 @@ class DropboxController
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector("#files");
         this.snackModalEl = document.querySelector("#react-snackbar-root");
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
         this.initEvents();
     }
 
@@ -41,6 +42,10 @@ class DropboxController
                 };
 
                 ajax.onerror = e => reject(e);
+
+                ajax.upload.onprogress = e => {
+                    this.uploadProgress(e, files);
+                }
                 
                 let formData = new FormData();
                 formData.append('input-file', file);
@@ -49,5 +54,14 @@ class DropboxController
         });
 
         return Promise.all(promises);
+    }
+
+    uploadProgress(event, files)
+    {
+        let loaded = event.loaded;
+        let total = event.total;
+        let percent = parseInt((total / loaded) * 100);
+
+        this.progressBarEl.style.width = `${percent}%`;
     }
 }
