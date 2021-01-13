7bc20a37421ae5b1d1364ae56db8bed5a0d1958f
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-13 08:02:50 -0300

    retirado os hífens das variaveis em desuso

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var formidable = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(_req, res, _next) {
   res.render('index', { title: 'Express' });
 });
 
