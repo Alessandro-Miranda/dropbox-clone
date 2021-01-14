e7896112d0dc270974b9510ab9553a931cf74700
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-14 10:13:41 -0300

    Criado o metódo para alterar os arquivos no firebase

--- app/public/src/controller/DropboxController.js
+++ app/public/src/controller/DropboxController.js
@@ -45,6 +45,19 @@ class DropboxController
 
     initEvents()
     {
+        this.btnRename.addEventListener('click', e => {
+            let li = this.getSelection()[0];
+            let file = JSON.parse(li.dataset.file);
+
+            let name = prompt("Renomear o arquivo", file.name);
+
+            if(name)
+            {
+                file.name = name;
+                this.getFirebaseRef().child(li.dataset.key).set(file);
+            }
+        })
+
         this.listFilesEl.addEventListener('selectionChange', () => {
             
             switch(this.getSelection().length)
@@ -357,6 +370,7 @@ class DropboxController
     {
         let li = document.createElement('li');
         li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
         li.innerHTML = `
             ${this.getFileIconView(file)}
             <div class="name text-center">${file.name}</div>
